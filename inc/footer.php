80d8790f8922eef9becad806de3118fd4e18b401
estelito02 <estelito02@gmail.com>
2019-05-11 14:51:00 -0300

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+</main><!-- /container -->
+<hr>
+<footer class="container">
+    <p>&copy;2017 – TPA CRUD</p>
+</footer>
+<script src="<?php echo BASEURL; ?>js/main.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php/** inclemento exercicio 7 */?>
+<?php require_once 'config.php'; ?>
+<?php require_once DBAPI; ?>
+<?php include(HEADER_TEMPLATE); ?>
+
+<?php/** inclemento exercicio 8 */?>
+<?php $db = open_database(); ?>
+    <h1>Painel de Controle</h1>
+    <hr />
+<?php if ($db) :
+    header("Content-Type: text/html; charset=utf8");
+?>
+
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 col-md-2">
+            <a href="customers/add.php" class="btn btn-primary">
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <i class="fa fa-plus fa-5x"></i>
+                    </div>
+                    <div class="col-xs-12 text-center">
+                        <p>Novo Cliente</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-sm-3 col-md-2">
+            <a href="customers" class="btn btn-default">
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <i class="fa fa-user fa-5x"></i>
+                    </div>
+                    <div class="col-xs-12 text-center">
+                        <p>Clientes</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+
+
+
+
+<?php else : ?>
+    <div class="alert alert-danger" role="alert">
+        <p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
+    </div>
+<?php endif; ?>
+
+
+<?php include(FOOTER_TEMPLATE); ?>
